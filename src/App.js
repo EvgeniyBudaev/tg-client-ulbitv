9d63ec9480fb6cceb3383c63ad13d9409b8fe897
Evgeniy Budaev <budaev.e@gmail.com>
2024-01-25 16:54:54 +0300

    fix: fixed build

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
 import {useEffect} from "react";
+import './App.css';
 
 function App() {
     const tg = window.Telegram.WebApp;
 
     useEffect(() => {
         tg.ready(); // ready сигнализирует, что приложение полностью проинициализировалось и его можно отрисовывать
-    }, []);
+    }, [tg]);
 
     const onClose = () => {
         tg.close();
