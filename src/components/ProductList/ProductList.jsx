d10757b740553d2fdc02913fce8782c11f34712c
Evgeniy Budaev <budaev.e@gmail.com>
2024-01-25 22:36:21 +0300

    feat: ProductList and ProductItem

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,8 +1,65 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
+import {useTelegram} from "../../hooks/useTelegram";
+import {products} from "./mockData";
+import {ProductItem} from "../ProductItem/ProductItem";
 import "./ProductList.css";
 
 export const ProductList = () => {
+    const [addedItems, setAddedItems] = useState([]);
+    const {tg, queryId} = useTelegram();
+
+    const onSendData = useCallback(() => {
+        const data = {
+            products: addedItems,
+            totalPrice: getTotalPrice(addedItems),
+            queryId,
+        }
+        fetch('http://localhost:8000/web-data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        });
+    }, [addedItems, queryId]);
+
+    useEffect(() => {
+        tg.onEvent("mainButtonClicked", onSendData);
+        return () => {
+            tg.offEvent("mainButtonClicked", onSendData);
+        }
+    }, [onSendData]);
+
+    const getTotalPrice = (items) => {
+        return items.reduce((acc, item) => {
+            return acc += item.price
+        }, 0);
+    };
+
+    const onAddHandler = (product) => {
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
+        if (alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id);
+        } else {
+            newItems = [...addedItems, product];
+        }
+        setAddedItems(newItems);
+        if (newItems.length === 0) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            });
+        }
+    }
+
     return (
-        <div></div>
+        <div className="list">
+            {products.map(item => (
+                <ProductItem className="item" key={item.id} product={item} onAdd={onAddHandler}/>
+            ))}
+        </div>
     );
 };
---
+++ src/components/ProductList/mockData.js
@@ -0,0 +1,4 @@
+export const products = [
+    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
+];
